3fee8788c49a0ca13f7a3e8d757a1fdac16cd0c4
Akarsh Gangwar <akarshprog28@gmail.com>
2022-04-06 00:57:12 +0530

    skeleton of todo app is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+  render()
+  {
+    return(
+      <div>
+        <img src={logo} width="80" height="80" className='logo'/>
+        <h1 className='app-title' > TODO APP</h1>
+        <div className="container">
+        
+
+        <input
+          type="text"
+          placeholder='Enter Task'
+          className='input-text'
+        />
+
+         <button 
+         className='add-btn'>Add Item
+         </button>
+
+         <div className='list'>
+          <ul>
+            <li>
+              <input type="checkbox" name=""  />
+              Buy some Milk
+              <button className='btn'>Delete</button>
+            </li>
+            </ul> 
+         
+         </div>
+         </div>
+        
+      </div>
+
+
+    );
+  }
 }
 
 export default App;
